0218fd4ffe860a63a35744c120e9e516ff8d19a6
Baha-Alawneh <bahaalawneh07@gmail.com>
2025-10-13 16:26:17 +0300

    login page

--- apis/user/SignUp.js
+++ apis/user/SignUp.js
@@ -1,27 +1,53 @@
+import axios from "axios";
 import { BASE_URL } from "../../constants/config";
 
 export const registerUser = async (body) => {
   try {
-    const response = await fetch(`${BASE_URL}/users/register`, {
-      method: "POST",
+    const response = await axios.post(`${BASE_URL}/users/register`, {
+      name: body.name,
+      email: body.email,
+      password: body.password,
+      role: body.role,
+    }, {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        name: body.name,
-        email: body.email,
-        password: body.password,
-        role: body.role,
-      }),
     });
 
-    const data = await response.json();
-    if (response.ok) {
-      return { success: true, message: `${body.name} registered successfully` };
-    } else {
-      return { success: false, message: data.message };
-    }
+    return { success: true, message: `${body.name} registered successfully` };
   } catch (error) {
-    return { success: false, message: error.message };
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      return { success: false, message: error.response.data.message || error.response.data };
+    } else if (error.request) {
+      // The request was made but no response was received
+      return { success: false, message: "No response from server" };
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      return { success: false, message: error.message };
+    }
+  }
+};
+export const loginUser = async (body) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/users/login`, {
+      email: body.email,
+      password: body.password,
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return { success: true, message: "Login successful", token: response.data.token };
+  } catch (error) {
+    if (error.response) {
+      return { success: false, message: error.response.data.message || error.response.data };
+    } else if (error.request) {
+      return { success: false, message: "No response from server" };
+    } else {
+      return { success: false, message: error.message };
+    }
   }
 };
--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,5 @@
-export const SERVER_IP = "192.168.1.98";
+export const SERVER_IP_AYMAN = "192.168.1.98";
+export const SERVER_IP_BAHA = "192.168.88.252";
 export const SERVER_PORT = "5000";
 
-export const BASE_URL = `http://${SERVER_IP}:${SERVER_PORT}`;
+export const BASE_URL = `http://${SERVER_IP_BAHA}:${SERVER_PORT}`;
