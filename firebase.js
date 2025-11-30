256328026f8d1c062ee93be50df2509601ffc3b0
Ayman Abu Hijleh <a2y2m2a2n@gmail.com>
2025-11-30 17:13:31 +0200

    update front

--- firebase.js
+++ firebase.js
@@ -15,5 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
+  persistence: getReactNativePersistence(AsyncStorage),
 });
