74db024c1c25bebd1ebb88fb0213c083ef34ddc9
Ayman Abu Hijleh <a2y2m2a2n@gmail.com>
2026-01-21 00:55:24 +0200

    fix some issues

--- constants/config.js
+++ constants/config.js
@@ -4,11 +4,6 @@ export const SERVER_IP_BAHA = "192.168.88.252";
 
 export const SERVER_PORT = "5000";
 
-<<<<<<< HEAD
 
 export const BASE_URL = `http://${SERVER_IP_HOME_AYMAN}:${SERVER_PORT}/api/v1`;
 export const API_URL = BASE_URL;
-=======
-export const BASE_URL = `http://${SERVER_IP_BAHA}:${SERVER_PORT}/api/v1`;
-export const API_URL = BASE_URL; 
->>>>>>> d173c14261647b36bed9f04e2fb533c90f95b7e2
