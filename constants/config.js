3b99d92ca4fa3081da71632b7d8665f1e3b2fa41
Ayman Abu Hijleh <a2y2m2a2n@gmail.com>
2026-01-24 12:47:25 +0200

    ip

--- constants/config.js
+++ constants/config.js
@@ -5,5 +5,5 @@ export const SERVER_IP_BAHA = "192.168.88.2";
 export const SERVER_PORT = "5000";
 
 
-export const BASE_URL = `http://${SERVER_IP_BAHA}:${SERVER_PORT}/api/v1`;
+export const BASE_URL = `http://${SERVER_IP_HOME_AYMAN}:${SERVER_PORT}/api/v1`;
 export const API_URL = BASE_URL;
